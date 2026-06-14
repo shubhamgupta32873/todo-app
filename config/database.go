9eb89ef0767546ea1470e1ac5123a3bb368b1a34
Shubham Gupta <shubhamboss32873@gmail.com>
2026-06-14 17:31:50 +0530

    Add database configuration module

---
+++ config/database.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+// InitDatabase initializes PostgreSQL connection
+func InitDatabase() {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	DB = db
+	log.Println("✅ Database connected successfully")
+}
+
+// GetDB returns the database instance
+func GetDB() *gorm.DB {
+	return DB
+}
